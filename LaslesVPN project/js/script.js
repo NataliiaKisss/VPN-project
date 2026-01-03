f9a7e1ed86e3c64ecc117a7cb1eea22b49bf751c
NataliiaKisss <nataliia.kisilishyna@gmail.com>
2026-01-03 14:19:07 +0200

    Add files via upload

---
+++ LaslesVPN project/js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  // spaceBetween: ,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
